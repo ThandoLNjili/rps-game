1e08598620598b1e9135a346f6bfead88e120e9b
Thandolwethu Njili <thandolnjili@gmail.com>
2019-10-01 16:21:31 +0200

    Updates

---
+++ precache-manifest.f00f900e0a86c68dda51efad559cfd95.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "225dcb6c6971501386248fb9a8660f7d",
+    "url": "/rps-game/index.html"
+  },
+  {
+    "revision": "ce941a432c0d63da1cd2",
+    "url": "/rps-game/static/css/main.296f6079.chunk.css"
+  },
+  {
+    "revision": "12412a1c9d10aad50b4f",
+    "url": "/rps-game/static/js/2.72557104.chunk.js"
+  },
+  {
+    "revision": "ce941a432c0d63da1cd2",
+    "url": "/rps-game/static/js/main.e38fac47.chunk.js"
+  },
+  {
+    "revision": "b179d5f479c937c77ab3",
+    "url": "/rps-game/static/js/runtime~main.f663d57f.js"
+  },
+  {
+    "revision": "619d1ee74ffbb884fb7fc21af4b77636",
+    "url": "/rps-game/static/media/paper.619d1ee7.png"
+  },
+  {
+    "revision": "602f85e5c6c60fed18a5e47896650db0",
+    "url": "/rps-game/static/media/rock.602f85e5.png"
+  },
+  {
+    "revision": "9defba7cc1cb8e4a7e16714a041d6635",
+    "url": "/rps-game/static/media/scissors.9defba7c.png"
+  }
+]);
